34489c9f95df4f536c1515c1e6a5405ab5962563
Aaisha Jha <jhaaaisha78@gmail.com>
2025-12-26 16:58:52 +0545

    Add Student class with displayInfo method
    Making Class and its instance, i.e Object

---
+++ Assignment -2/Student.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Student {
+    int rollNumber;
+    String name;
+
+    void displayInfo() {
+        System.out.println("Roll: " + rollNumber);
+        System.out.println("Name: " + name);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.rollNumber = 1;
+        s1.name = "Ram";
+        s1.displayInfo();
+    }
+}
