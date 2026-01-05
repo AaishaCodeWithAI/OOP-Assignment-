d774bf7a37e732ccf6b8e244d1ffee0f2a53a076
Aaisha Jha <jhaaaisha78@gmail.com>
2026-01-05 12:26:30 +0545

    Update RightTriangle.java

--- Assignment -2/Tutorial/RightTriangle.java
+++ Assignment -2/Tutorial/RightTriangle.java
@@ -2,29 +2,31 @@ package Assignment2.Tutorials;
 
 import java.util.Scanner;
 
-public class RightTriangle {
+public class SwapWithoutThird {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         char choice;
 
         do {
-            System.out.print("Enter number of rows: ");
-            int rows = sc.nextInt();
+            System.out.print("Enter first number (a): ");
+            int a = sc.nextInt();
 
-            if (rows <= 0) {
-                System.out.println("Number of rows must be positive!");
-            } else {
-                System.out.println("Right-Angled Triangle Pattern:");
-                for (int i = 1; i <= rows; i++) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print("*");
-                    }
-                    System.out.println();
-                }
-            }
+            System.out.print("Enter second number (b): ");
+            int b = sc.nextInt();
 
-            System.out.print("Do you want to print another triangle? (y/n): ");
+            System.out.println("\nBefore swapping:");
+            System.out.println("a = " + a + ", b = " + b);
+
+            // Swap without third variable
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            System.out.println("After swapping:");
+            System.out.println("a = " + a + ", b = " + b);
+
+            System.out.print("\nDo you want to swap another pair of numbers? (y/n): ");
             choice = sc.next().charAt(0);
 
         } while (choice == 'y' || choice == 'Y');
