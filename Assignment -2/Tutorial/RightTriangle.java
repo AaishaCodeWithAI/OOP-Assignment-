817107a16f4cdba005ef46c63e96ce43cb72e7e1
Aaisha Jha <jhaaaisha78@gmail.com>
2026-01-05 12:25:10 +0545

    Update RightTriangle.java

--- Assignment -2/Tutorial/RightTriangle.java
+++ Assignment -2/Tutorial/RightTriangle.java
@@ -1,18 +1,35 @@
 package Assignment2.Tutorials;
 
 import java.util.Scanner;
+
 public class RightTriangle {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        char choice;
 
-        System.out.print("Enter number of rows: ");
-        int rows = sc.nextInt();
+        do {
+            System.out.print("Enter number of rows: ");
+            int rows = sc.nextInt();
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            if (rows <= 0) {
+                System.out.println("Number of rows must be positive!");
+            } else {
+                System.out.println("Right-Angled Triangle Pattern:");
+                for (int i = 1; i <= rows; i++) {
+                    for (int j = 1; j <= i; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
             }
-            System.out.println();
-        }
+
+            System.out.print("Do you want to print another triangle? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("Program ended. Thank you!");
+        sc.close();
     }
 }
