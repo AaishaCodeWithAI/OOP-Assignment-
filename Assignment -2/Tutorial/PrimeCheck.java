62238bc456340aab036a71a9b0fbef6069139fa2
Aaisha Jha <jhaaaisha78@gmail.com>
2026-01-05 12:20:37 +0545

    Update PrimeCheck.java

--- Assignment -2/Tutorial/PrimeCheck.java
+++ Assignment -2/Tutorial/PrimeCheck.java
@@ -1,30 +1,42 @@
 package Assignment2.Tutorials;
 
 import java.util.Scanner;
+
 public class PrimeCheck {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        char choice;
 
-        System.out.print("Enter number: ");
-        int num = sc.nextInt();
+        do {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
 
-        boolean isPrime = true;
+            boolean isPrime = true;
 
-        if (num <= 1)
-            isPrime = false;
-        else {
-            for (int i = 2; i <= num / 2; i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
+            if (num <= 1) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(num); i++) {
+                    if (num % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
                 }
             }
-        }
 
-        if (isPrime)
-            System.out.println("Prime Number");
-        else
-            System.out.println("Not a Prime Number");
+            if (isPrime)
+                System.out.println("Prime Number");
+            else
+                System.out.println("Not a Prime Number");
+
+            System.out.print("Do you want to check another number? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("Program ended. Thank you!");
+        sc.close();
     }
 }
 
