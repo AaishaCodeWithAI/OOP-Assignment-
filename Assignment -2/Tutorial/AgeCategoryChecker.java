602475700c14074782fc4519b64ada0acb89c9a8
Aaisha Jha <jhaaaisha78@gmail.com>
2026-01-05 09:36:40 +0545

    Update AgeCategoryChecker.java

--- Assignment -2/Tutorial/AgeCategoryChecker.java
+++ Assignment -2/Tutorial/AgeCategoryChecker.java
@@ -1,18 +1,33 @@
 package Assignment2.Tutorials;
 
 import java.util.Scanner;
+
 public class AgeCategoryChecker {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        char choice;
 
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
+        do {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
 
-        if (age < 18)
-            System.out.println("You are a Minor");
-        else if (age < 60)
-            System.out.println("You are a Adult");
-        else
-            System.out.println("You are a Senior");
+            if (age < 0) {
+                System.out.println("Invalid age! Age cannot be negative.");
+            } else if (age < 18) {
+                System.out.println("You are a Minor");
+            } else if (age < 60) {
+                System.out.println("You are an Adult");
+            } else {
+                System.out.println("You are a Senior");
+            }
+
+            System.out.print("Do you want to check another age? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("Program ended. Thank you!");
+        sc.close();
     }
 }
