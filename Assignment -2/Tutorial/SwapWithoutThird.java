1921ce3316a2337dd5c6df016c55fccad7e78ef1
Aaisha Jha <jhaaaisha78@gmail.com>
2026-02-02 21:30:31 +0545

    Update SwapWithoutThird.java
    Added Exception Handling

--- Assignment -2/Tutorial/SwapWithoutThird.java
+++ Assignment -2/Tutorial/SwapWithoutThird.java
@@ -9,30 +9,35 @@ public class SwapWithoutThird {
         char choice;
 
         do {
-            System.out.print("Enter first number (a): ");
-            int a = sc.nextInt();
+            try {
+                System.out.print("Enter first number (a): ");
+                int a = sc.nextInt();
 
-            System.out.print("Enter second number (b): ");
-            int b = sc.nextInt();
+                System.out.print("Enter second number (b): ");
+                int b = sc.nextInt();
 
-            System.out.println("\nBefore swapping:");
-            System.out.println("a = " + a + ", b = " + b);
+                System.out.println("\nBefore swapping:");
+                System.out.println("a = " + a + ", b = " + b);
 
-            // Swap without third variable
-            a = a + b;
-            b = a - b;
-            a = a - b;
+                // Swap without third variable
+                a = a + b;
+                b = a - b;
+                a = a - b;
 
-            System.out.println("After swapping:");
-            System.out.println("a = " + a + ", b = " + b);
+                System.out.println("After swapping:");
+                System.out.println("a = " + a + ", b = " + b);
+            }catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter an integer value.");
+                sc.next(); // clear invalid input
+            }
 
-            System.out.print("\nDo you want to swap another pair of numbers? (y/n): ");
-            choice = sc.next().charAt(0);
+                System.out.print("\nDo you want to swap another pair of numbers? (y/n): ");
+                choice = sc.next().charAt(0);
+                
+                while (choice == 'y' || choice == 'Y');
 
-        } while (choice == 'y' || choice == 'Y');
-
-        System.out.println("Program ended. Thank you!");
-        sc.close();
-    }
+                System.out.println("Program ended. Thank you!");
+                sc.close();
+      }
+   }
 }
-
