c8a4030954524d05eb2b0e05fcbdc431171035d9
Aaisha Jha <jhaaaisha78@gmail.com>
2026-02-02 21:19:07 +0545

    Update PyramidPattern.java
    Added Exception Handling

--- Assignment -2/Tutorial/PyramidPattern.java
+++ Assignment -2/Tutorial/PyramidPattern.java
@@ -9,22 +9,27 @@ public class PyramidPattern {
         char choice;
 
         do {
-            System.out.print("Enter number of rows: ");
-            int rows = sc.nextInt();
+             try {
+                 System.out.print("Enter number of rows: ");
+                 int rows = sc.nextInt();
 
-            if (rows <= 0) {
+                if (rows <= 0) {
                 System.out.println("Number of rows must be positive!");
-            } else {
-                for (int i = 1; i <= rows; i++) {
-                    for (int space = 1; space <= rows - i; space++) {
+                } else {
+                   for (int i = 1; i <= rows; i++) {
+                      for (int space = 1; space <= rows - i; space++) {
                         System.out.print(" ");
-                    }
-                    for (int star = 1; star <= (2 * i - 1); star++) {
+                      }
+                      for (int star = 1; star <= (2 * i - 1); star++) {
                         System.out.print("*");
-                    }
-                    System.out.println();
+                      }
+                       System.out.println();
+                   }
+                } catch (InputMismatchException e) {
+                
+                System.out.println("Invalid input! Please enter an integer value.");
+                sc.next(); // clear invalid input
                 }
-            }
 
             System.out.print("Do you want to print another pyramid? (y/n): ");
             choice = sc.next().charAt(0);
