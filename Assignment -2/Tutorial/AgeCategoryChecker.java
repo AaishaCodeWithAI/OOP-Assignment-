af37d43c7836c294da519d818fb2aef14a50836a
Aaisha Jha <jhaaaisha78@gmail.com>
2026-02-02 21:01:31 +0545

    Update AgeCategoryChecker.java
    Checking the age categories while handling the exception case of Input mismatch.

--- Assignment -2/Tutorial/AgeCategoryChecker.java
+++ Assignment -2/Tutorial/AgeCategoryChecker.java
@@ -1,25 +1,32 @@
 package Assignment2.Tutorials;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AgeCategoryChecker {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        char choice;
+        char choice = 'y';
 
         do {
-            System.out.print("Enter your age: ");
-            int age = sc.nextInt();
+            try {
+                System.out.print("Enter your age: ");
+                int age = sc.nextInt();
 
-            if (age < 0) {
-                System.out.println("Invalid age! Age cannot be negative.");
-            } else if (age < 18) {
-                System.out.println("You are a Minor");
-            } else if (age < 60) {
-                System.out.println("You are an Adult");
-            } else {
-                System.out.println("You are a Senior");
+                if (age < 0) {
+                    System.out.println("Invalid age! Age cannot be negative.");
+                } else if (age < 18) {
+                    System.out.println("You are a Minor");
+                } else if (age < 60) {
+                    System.out.println("You are an Adult");
+                } else {
+                    System.out.println("You are a Senior");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter vaild age numbers only.");
+                sc.next(); // clear wrong input
             }
 
             System.out.print("Do you want to check another age? (y/n): ");
